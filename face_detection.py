e0c8bc29eb836ccc310be67cf27f37862c7f9f00
Dhruv Diddi <dhruv.diddi+1@gmail.com>
2019-05-22 11:59:13 -0700

    Create rectangles in output frame around face detections

--- face_detection.py
+++ face_detection.py
@@ -1,12 +1,10 @@
 import cv2
 import sys
 
-
-
 def detect_face_openCV(face_classifier, frame, height=300,width=0):
-    temp_frame = frame.copy()
-    frame_height = temp_frame.shape[0]
-    frame_width = temp_frame.shape[1]
+    output_frame = frame.copy()
+    frame_height = output_frame.shape[0]
+    frame_width = output_frame.shape[1]
 
     if not width:
         width = int((frame_width/frame_height)*height)
@@ -14,12 +12,19 @@ def detect_face_openCV(face_classifier, frame, height=300,width=0):
     # Create scale variables to comensate for classifer and image dimensions
     scale_height = frame_height/height
     scale_width = frame_width/width
-    scale_frame = cv2.resize(temp_frame, (width,height))
+    scale_frame = cv2.resize(output_frame, (width,height))
 
     # Convert frame to gray for face detection classifier input
     gray_frame = cv2.cvtColor(scale_frame, cv2.COLOR_BGR2GRAY)
 
+    faces = face_classifier.detectMultiScale(gray_frame)
 
+    # Put box at face detection areas
+    for (x,y,w,h) in faces:
+        rect = [int(x*scale_width),int(y*scale_height),int((x+w)*scale_width),int((y+h)*scale_height)]
+        cv2.rectangle(output_frame,(rect[0], rect[1]), (rect[2], rect[3]), (0, 255, 0),
+                      int(round(frame_height / 150)), 4)
+    return output_frame
 
 # Get input video from system arguments
 input_video = sys.argv[1]
